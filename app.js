e0d8d2abb9d850c1f15493ea9520cd817ee9ac54
Victor Garcia Mestre <vgmestre@gmail.com>
2021-04-18 02:50:45 +0200

    restylized navigation bar

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navLinks = document.querySelectorAll(".nav__link");
+const navClose = document.getElementById("nav__close-layer");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
@@ -9,4 +10,8 @@ navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav-open");
   });
+});
+
+navClose.addEventListener("click", () => {
+  document.body.classList.remove("nav-open");
 });
